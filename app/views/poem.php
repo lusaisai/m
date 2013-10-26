3f0e55118ba6f054cb2a7302cedbde164af6003c
lusaisai <lusaisai@163.com>
2013-10-26 23:03:18 +0800

    modify the css of the player

--- app/views/poem.php
+++ app/views/poem.php
@@ -1,10 +1,6 @@
 <?php
-$query = "select content, poet from poems order by rand() limit 1";
-$row = Database::first($query);
-if (empty($row->poet)) {
-	echo $row->content;
-} else {
-	echo $row->content . " - " . $row->poet;
-}
+$query = "select content from poems order by rand() limit 1";
+$content = Database::column($query);
+echo $content;
  ?>
  
